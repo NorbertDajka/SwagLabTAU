81dcda6f609453be57d83ec7c304efd1d90baf4d
NorbertDajka <dajkas2005@gmail.com>
2024-11-15 14:50:03 +0200

    Added to New LoginTest - LoginWithNoCredentials

--- src/main/java/norbert/dajka/Body/LoginModal.java
+++ src/main/java/norbert/dajka/Body/LoginModal.java
@@ -4,15 +4,24 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
 
-public class Login {
+public class LoginModal {
 
     private final SelenideElement userField = $("#user-name");
     private final SelenideElement passwordField = $("#password");
     private final SelenideElement loginButton = $("#login-button");
+    private final SelenideElement errorMsg = $(".error-message-container");
 
     public void login(String username, String password) {
         userField.setValue(username);
         passwordField.setValue(password);
         loginButton.click();
+    }
+
+    public boolean errorMsgExistsAndVisible() {
+        return this.errorMsg.exists() & errorMsg.isDisplayed();
+    }
+
+    public String getErrorMsg() {
+        return errorMsg.text();
     }
 }
--- src/test/java/norbert/dajka/Tests/LoginModalTest.java
+++ src/test/java/norbert/dajka/Tests/LoginModalTest.java
@@ -1,6 +1,6 @@
 package norbert.dajka.Tests;
 
-import norbert.dajka.Body.Login;
+import norbert.dajka.Body.LoginModal;
 import norbert.dajka.Body.SwagLabsPage;
 import norbert.dajka.Pages.ShoppingPage;
 import norbert.dajka.config.BaseTestConfig;
@@ -8,16 +8,16 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTest extends BaseTestConfig {
+public class LoginModalTest extends BaseTestConfig {
 
     SwagLabsPage page;
-    Login login;
+    LoginModal loginModal;
 
 @BeforeMethod
 
     public void setUp()  {
         page = new SwagLabsPage();
-        login = new Login();
+        loginModal = new LoginModal();
         this.page.openSwaglabsUrl();
 
 }
@@ -25,10 +25,18 @@ public class LoginTest extends BaseTestConfig {
 @Test
     public void testLoginWithValidCredentials()  {
 
-    this.login.login("standard_user","secret_sauce");
+    loginModal.login("standard_user","secret_sauce");
 
     ShoppingPage sPage = new ShoppingPage();
     Assert.assertTrue(sPage.isShoppingCartBadgeVisible(),"Login is functional");
+    }
+@Test
+    public void testLoginWithNoCredentials(){
+
+    loginModal.login("","");
+    Assert.assertTrue(loginModal.errorMsgExistsAndVisible());
+    Assert.assertEquals(loginModal.getErrorMsg(),"Epic sadface: Username is required");
+
     }
 
 }
